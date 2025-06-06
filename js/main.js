abaa6d7602598535fe1c54f7597987275daf0668
Ashik <ashikshettyc@gmail.com>
2025-06-06 14:31:19 +0530

    content update

--- js/main.js
+++ js/main.js
@@ -6,178 +6,182 @@
 -------------------------------------------------------------------
 */
 
- (function($) {
-     "use strict";
+(function ($) {
+  'use strict';
 
+  /*======== Doucument Ready Function =========*/
+  jQuery(document).ready(function () {
+    //CACHE JQUERY OBJECTS
+    $('#status').fadeOut();
+    $('#preloader').delay(200).fadeOut('slow');
+    $('body').delay(200).css({ overflow: 'visible' });
 
-      /*======== Doucument Ready Function =========*/
-    jQuery(document).ready(function () {
-     //CACHE JQUERY OBJECTS
-      $("#status").fadeOut();
-      $("#preloader").delay(200).fadeOut("slow");
-      $("body").delay(200).css({ "overflow": "visible" });
+    /* Init Wow Js */
+    new WOW().init();
+  });
 
-      
-      /* Init Wow Js */
-      new WOW().init();
+  //search categories
+  $('a[href="#search1"]').on('click', function (event) {
+    event.preventDefault();
+    $('#search1').addClass('open');
+    $('#search1 > form > input[type="search"]').focus();
+  });
+  $('#search1, #search1 button.close').on('click keyup', function (event) {
+    if (
+      event.target == this ||
+      event.target.className == 'close' ||
+      event.keyCode == 27
+    ) {
+      $(this).removeClass('open');
+    }
+  });
 
-    });
-
-     //search categories
-    $('a[href="#search1"]').on('click', function(event) {
-         event.preventDefault();
-         $('#search1').addClass('open');
-         $('#search1 > form > input[type="search"]').focus();
-     });
-     $('#search1, #search1 button.close').on('click keyup', function(event) {
-         if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
-             $(this).removeClass('open');
-         }
-     });
-
-
-     /* ------------------------------------------------------------------------ */
-     /* BACK TO TOP
+  /* ------------------------------------------------------------------------ */
+  /* BACK TO TOP
     /* ------------------------------------------------------------------------ */
-     $(document).on('click', '#back-to-top, .back-to-top', () => {
-         $('html, body').animate({
-             scrollTop: 0
-         }, '500');
-         return false;
-     });
-     $(window).on('scroll', () => {
-         if ($(window).scrollTop() > 500) {
-             $('#back-to-top').fadeIn(200);
-         } else {
-             $('#back-to-top').fadeOut(200);
-         }
-     });
+  $(document).on('click', '#back-to-top, .back-to-top', () => {
+    $('html, body').animate(
+      {
+        scrollTop: 0,
+      },
+      '500'
+    );
+    return false;
+  });
+  $(window).on('scroll', () => {
+    if ($(window).scrollTop() > 500) {
+      $('#back-to-top').fadeIn(200);
+    } else {
+      $('#back-to-top').fadeOut(200);
+    }
+  });
 
-     /*-----------------------------------------------------------------------------------*/
-    /*  MASONRY
+  /*-----------------------------------------------------------------------------------*/
+  /*  MASONRY
     /*-----------------------------------------------------------------------------------*/
-    
-    $('.gallery-main').masonry({
-        // options
-        itemSelector: '.mansonry-item',
-    });
 
-     // Slick SLider
+  $('.gallery-main').masonry({
+    // options
+    itemSelector: '.mansonry-item',
+  });
 
-     $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav'
+  // Slick SLider
+
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav',
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    dots: false,
+    arrows: false,
+    centerMode: true,
+    focusOnSelect: true,
+  });
+
+  $('.review-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    rows: 0,
+    autoplay: true,
+    speed: 2000,
+    loop: true,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+        },
+      },
+    ],
+  });
+
+  $('#contactform2').validate({
+    submitHandler: function () {
+      $.ajax({
+        url: 'mail/contact.php',
+        type: 'POST',
+        data: {
+          fname: $('input[name="first_name"]').val(),
+          lname: $('input[name="last_name"]').val(),
+          email: $('input[name="email"]').val(),
+          phone: $('input[name="phone"]').val(),
+          comments: $('textarea[name="comments"]').val(),
+        },
+        success: function (result) {
+          $('#contactform-error-msg').html(result);
+          $('#contactform2')[0].reset();
+        },
       });
-      $('.slider-nav').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: true,
-        dots: false,
-        arrows: false,
-        centerMode: true,
-        focusOnSelect: true,
-      });
+    },
+  });
 
-     $('.review-slider').slick({
-         infinite: true,
-         slidesToShow: 1,
-         slidesToScroll: 1,
-         arrows: false,
-         dots: false,
-         rows:0,
-         autoplay: true,
-         speed: 2000,
-         loop:true,
-         responsive: [{
-             breakpoint: 991,
-             settings: {
-                 slidesToShow: 1,
-                 arrows: false,
-             }
-         }]
-     });
-
-
-     $("#contactform2").validate({      
-      submitHandler: function() {
-        
-        $.ajax({
-          url : 'mail/contact.php',
-          type : 'POST',
-          data : {
-            fname : $('input[name="first_name"]').val(),
-            lname : $('input[name="last_name"]').val(),
-            email : $('input[name="email"]').val(),
-            phone : $('input[name="phone"]').val(),
-            comments : $('textarea[name="comments"]').val(),
-          },
-          success : function( result ){
-            $('#contactform-error-msg').html( result );
-            $("#contactform2")[0].reset();
-          }     
-        });
-
-      }
-    });
-
-    
-     /*-----------------------------------------------------------------------------------*/
-    /*  COUNTDOWN
+  /*-----------------------------------------------------------------------------------*/
+  /*  COUNTDOWN
     /*-----------------------------------------------------------------------------------*/
 
-     $(document).ready(() => {
-         loopcounter('coming-counter');
-     });
+  //  $(document).ready(() => {
+  //      loopcounter('coming-counter');
+  //  });
 
+  /*-----------------------------------------------------------------------------------*/
+  /*  COUNTER UP
     /*-----------------------------------------------------------------------------------*/
-    /*  COUNTER UP
-    /*-----------------------------------------------------------------------------------*/
-    $('.value').counterUp({
-        delay: 50,
-        time: 1000
-    });
+  $('.value').counterUp({
+    delay: 50,
+    time: 1000,
+  });
 
-    $('.selector4').animatedHeadline({
-        animationType: 'rotate-3'
-    });
+  $('.selector4').animatedHeadline({
+    animationType: 'rotate-3',
+  });
 
+  // Nice Select JS
+  $('.niceSelect').niceSelect();
 
-     // Nice Select JS
-     $('.niceSelect').niceSelect();
+  $('a[href="#search1"]').on('click', function (event) {
+    event.preventDefault();
+    $('#search1').addClass('open');
+    $('#search1 > form > input[type="search"]').focus();
+  });
+  $('#search1, #search1 button.close').on('click keyup', function (event) {
+    if (
+      event.target == this ||
+      event.target.className == 'close' ||
+      event.keyCode == 27
+    ) {
+      $(this).removeClass('open');
+    }
+  });
+  //Do not include! This prevents the form from submitting for DEMO purposes only!
+  $('form').submit(function (event) {
+    event.preventDefault();
+    return false;
+  });
+})(jQuery);
 
-     $('a[href="#search1"]').on('click', function(event) {
-         event.preventDefault();
-         $('#search1').addClass('open');
-         $('#search1 > form > input[type="search"]').focus();
-     });
-     $('#search1, #search1 button.close').on('click keyup', function(event) {
-         if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
-             $(this).removeClass('open');
-         }
-     });
-     //Do not include! This prevents the form from submitting for DEMO purposes only!
-     $('form').submit(function(event) {
-         event.preventDefault();
-         return false;
-     });
-
- })(jQuery);
-
-
- jQuery(window).on('resize load', () => {
-     resize_eb_slider();
- }).resize();
- /**
-  * Resize slider
-  */
- function resize_eb_slider() {
-     let bodyheight = jQuery(this).height();
-     if (jQuery(window).width() > 1400) {
-         bodyheight *= 0.90;
-         jQuery('.slider').css('height', `${bodyheight}px`);
-     }
- }
+jQuery(window)
+  .on('resize load', () => {
+    resize_eb_slider();
+  })
+  .resize();
+/**
+ * Resize slider
+ */
+function resize_eb_slider() {
+  let bodyheight = jQuery(this).height();
+  if (jQuery(window).width() > 1400) {
+    bodyheight *= 0.9;
+    jQuery('.slider').css('height', `${bodyheight}px`);
+  }
+}

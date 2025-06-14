05515d8ea188b8622f2b9865d7f388e9599a0c29
Ashik <ashikshettyc@gmail.com>
2025-06-14 16:31:48 +0530

    changes

--- js/custom-nav.js
+++ js/custom-nav.js
@@ -1,105 +1,106 @@
-$(window).on('scroll', function(event) {
-    var scrollValue = $(window).scrollTop();
-    if (scrollValue > 70) {
-         $('.header_menu').addClass('fixed-top animated slideInDown');
-    } else{
-      $('.header_menu').removeClass('fixed-top animated slideInDown');
-    } 
+$(window).on('scroll', function (event) {
+  var scrollValue = $(window).scrollTop();
+  if (scrollValue > 70) {
+    $('.header_menu').addClass('fixed-top animated slideInDown');
+  } else {
+    $('.header_menu').removeClass('fixed-top animated slideInDown');
+  }
 });
 
-
-  
-"use strict";
-
+('use strict');
 
 /*======== Doucument Ready Function =========*/
 jQuery(document).ready(function () {
+  // slicknav
+  /**
+   * Slicknav - a Mobile Menu
+   */
+  var $slicknav_label;
+  $('.responsive-menu').slicknav({
+    duration: 500,
+    easingOpen: 'easeInExpo',
+    easingClose: 'easeOutExpo',
+    closedSymbol: '<i class="fa fa-plus"></i>',
+    openedSymbol: '<i class="fa fa-minus"></i>',
+    prependTo: '#slicknav-mobile',
+    allowParentLinks: true,
+    label: '',
+  });
 
-      // slicknav
-    /**
-     * Slicknav - a Mobile Menu
-     */
-    var $slicknav_label;
-    $('.responsive-menu').slicknav({
-      duration: 500,
-      easingOpen: 'easeInExpo',
-      easingClose: 'easeOutExpo',
-      closedSymbol: '<i class="fa fa-plus"></i>',
-      openedSymbol: '<i class="fa fa-minus"></i>',
-      prependTo: '#slicknav-mobile',
-      allowParentLinks: true,
-      label:"" 
-    });
+  var $slicknav_label;
+  $('#responsive-menu').slicknav({
+    duration: 500,
+    easingOpen: 'easeInExpo',
+    easingClose: 'easeOutExpo',
+    closedSymbol: '<i class="fa fa-plus"></i>',
+    openedSymbol: '<i class="fa fa-minus"></i>',
+    prependTo: '#slicknav-mobile',
+    allowParentLinks: true,
+    label: '',
+  });
 
-    var $slicknav_label;
-    $('#responsive-menu').slicknav({
-      duration: 500,
-      easingOpen: 'easeInExpo',
-      easingClose: 'easeOutExpo',
-      closedSymbol: '<i class="fa fa-plus"></i>',
-      openedSymbol: '<i class="fa fa-minus"></i>',
-      prependTo: '#slicknav-mobile',
-      allowParentLinks: true,
-      label:"" 
-    });
+  /**
+   * Sticky Header
+   */
 
-    
-    /**
-     * Sticky Header
-     */
-        
-    $(window).scroll(function(){
-
-      if( $(window).scrollTop() > 10 ){
-
-        $('.navbar').addClass('navbar-sticky-in')
-
-      } else {
-        $('.navbar').removeClass('navbar-sticky-in')
-      }
-
-    })
-    
-    /**
-     * Main Menu Slide Down Effect
-     */
-     
-    var selected = $('#navbar li');
-    // Mouse-enter dropdown
-    selected.on("mouseenter", function() {
-        $(this).find('ul').first().stop(true, true).delay(350).slideDown(500, 'easeInOutQuad');
-    });
-
-    // Mouse-leave dropdown
-    selected.on("mouseleave", function() {
-        $(this).find('ul').first().stop(true, true).delay(100).slideUp(150, 'easeInOutQuad');
-    });
-
-    /**
-     *  Arrow for Menu has sub-menu
-     */
-    if ($(window).width() > 992) {
-      $(".navbar-arrow ul ul > li").has("ul").children("a").append("<i class='arrow-indicator fa fa-angle-right'></i>");
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 10) {
+      $('.navbar').addClass('navbar-sticky-in');
+    } else {
+      $('.navbar').removeClass('navbar-sticky-in');
     }
+  });
 
+  /**
+   * Main Menu Slide Down Effect
+   */
+
+  var selected = $('#navbar li');
+  // Mouse-enter dropdown
+  selected.on('mouseenter', function () {
+    $(this)
+      .find('ul')
+      .first()
+      .stop(true, true)
+      .delay(350)
+      .slideDown(500, 'easeInOutQuad');
+  });
+
+  // Mouse-leave dropdown
+  selected.on('mouseleave', function () {
+    $(this)
+      .find('ul')
+      .first()
+      .stop(true, true)
+      .delay(100)
+      .slideUp(150, 'easeInOutQuad');
+  });
+
+  /**
+   *  Arrow for Menu has sub-menu
+   */
+  if ($(window).width() > 992) {
+    $('.navbar-arrow ul ul > li')
+      .has('ul')
+      .children('a')
+      .append("<i class='arrow-indicator fa fa-angle-right'></i>");
+  }
 });
 
+(function () {
+  var doc = document.documentElement;
+  var w = window;
 
- (function(){
-
-    var doc = document.documentElement;
-    var w   = window;
-
-    /*
+  /*
     define four variables: curScroll, prevScroll, curDirection, prevDirection
     */
 
-    var curScroll;
-    var prevScroll = w.scrollY || doc.scrollTop;
-    var curDirection = 0;
-    var prevDirection = 0;
+  var curScroll;
+  var prevScroll = w.scrollY || doc.scrollTop;
+  var curDirection = 0;
+  var prevDirection = 0;
 
-    /*
+  /*
     how it works:
     -------------
     create a scroll event listener
@@ -115,50 +116,43 @@ jQuery(document).ready(function () {
     step 3: user scrolls up: curDirection 1, prevDirection 2 > show header
     */
 
-    var header = document.getElementById('header_menu');
-    var toggled;
-    var threshold = 200;
+  var header = document.getElementById('header_menu');
+  var toggled;
+  var threshold = 200;
 
-    var checkScroll = function() {
-        curScroll = w.scrollY || doc.scrollTop;
-        if(curScroll > prevScroll) {
-            // scrolled down
-            curDirection = 2;
-        }
-        else {
-            //scrolled up
-            curDirection = 1;
-        }
+  var checkScroll = function () {
+    curScroll = w.scrollY || doc.scrollTop;
+    if (curScroll > prevScroll) {
+      // scrolled down
+      curDirection = 2;
+    } else {
+      //scrolled up
+      curDirection = 1;
+    }
 
-        if(curDirection !== prevDirection) {
-            toggled = toggleHeader();
-        }
+    // if(curDirection !== prevDirection) {
+    //     toggled = toggleHeader();
+    // }
 
-        prevScroll = curScroll;
-        if(toggled) {
-            prevDirection = curDirection;
-        }
-    };
+    prevScroll = curScroll;
+    if (toggled) {
+      prevDirection = curDirection;
+    }
+  };
 
-    var toggleHeader = function() { 
-        toggled = true;
-        if(curDirection === 2 && curScroll > threshold) {
-            header.classList.add('hide');
-            jQuery('.sticky1').addClass('tab-sticky');
-        }
-        else if (curDirection === 1) {
-            header.classList.remove('hide');
-            jQuery('.sticky1').removeClass('tab-sticky');
-        }
-        else {
-            toggled = false;
-        }
-        return toggled;
-    };
+  var toggleHeader = function () {
+    toggled = true;
+    if (curDirection === 2 && curScroll > threshold) {
+      header.classList.add('hide');
+      jQuery('.sticky1').addClass('tab-sticky');
+    } else if (curDirection === 1) {
+      header.classList.remove('hide');
+      jQuery('.sticky1').removeClass('tab-sticky');
+    } else {
+      toggled = false;
+    }
+    return toggled;
+  };
 
-    window.addEventListener('scroll', checkScroll);
-
+  window.addEventListener('scroll', checkScroll);
 })();
-
-
-
